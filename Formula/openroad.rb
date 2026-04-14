243ab7be51e6a697634fe55366187b11b175c2e6
gange <william20040911@gmail.com>
2026-04-14 19:18:18 +0800

    openroad: bump revision for 2026.04.14-r1 tarball

--- Formula/openroad.rb
+++ Formula/openroad.rb
@@ -1,9 +1,11 @@
 class Openroad < Formula
   desc "OpenROAD EDA tool for Apple Silicon"
   homepage "https://the-openroad-project.org"
-  url "https://github.com/GangeWang/OpenROAD-build-for-apple-silicon/releases/download/v.2026.2.26/-arm64-mac.tar.gz"
-  version "2026.2.26"
-  sha256 "9cf62dfa3679d0cf2ff9d6445117e66b0ef72065959d3910a4d4a4090536dbe6"
+  url "https://github.com/GangeWang/OpenROAD-build-for-apple-silicon/releases/download/2026.04.14/openroad-arm64-mac-2026.04.14-r1.tar.gz"
+  version "2026.04.14"
+  revision 1
+  sha256 "71050f00b35e87a60a9f461ae1abcdcb8893e9d875e5608654a2034a3a30a737"
+
   depends_on "abseil"
   depends_on "cbc"
   depends_on "fmt"
@@ -13,6 +15,7 @@ class Openroad < Formula
   depends_on "libomp"
   depends_on "mpfr"
   depends_on "openblas"
+  depends_on "or-tools"
   depends_on "protobuf"
   depends_on "python@3.14"
   depends_on "qt@5"
@@ -22,12 +25,12 @@ class Openroad < Formula
   depends_on "tcl-tk@8"
   depends_on "yaml-cpp"
   depends_on "zstd"
-  depends_on "or-tools"
+
   def install
     prefix.install Dir["*"]
   end
 
   test do
-    assert_match "OpenROAD", shell_output("#{bin}/openroad -h")
+    assert_match "OpenROAD", shell_output("#{bin}/openroad -version")
   end
 end
